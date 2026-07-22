63301124d73fe8919481146704b594dd67e68064
Ryan Newton <rrnewton@gmail.com>
2026-07-22 11:48:16 -0700

    Allow intentional invalid pointer in select ABI test

--- detcore/tests/misc/mod.rs
+++ detcore/tests/misc/mod.rs
@@ -552,6 +552,7 @@ fn select_ready_and_timeout() {
 }
 
 #[test]
+#[allow(clippy::manual_dangling_ptr)]
 fn select_fd_set_abi_boundaries() {
     det_test_fn_without_pmu(|| {
         let invalid_set = 1_usize as *mut libc::fd_set;
