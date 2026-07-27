ec61a530707d01d305c9bc82a3ec27eade3a1765
Ryan Newton <rrnewton@users.noreply.github.com>
2026-07-27 07:39:55 -0700

    Update pinned-space test for reserved gauge

--- detcore/src/procfs.rs
+++ detcore/src/procfs.rs
@@ -1316,7 +1316,6 @@ mod tests {
             "/sys/fs/btrfs/63152D54-3f28-408a-80a2-46e53b5c0bda/allocation/data/bytes_pinned",
             "/sys/fs/btrfs/not-a-uuid/allocation/data/bytes_pinned",
             "/sys/fs/btrfs/63152d54-3f28-408a-80a2-46e53b5c0bda/allocation/global/bytes_pinned",
-            "/sys/fs/btrfs/63152d54-3f28-408a-80a2-46e53b5c0bda/allocation/data/bytes_reserved",
             "/sys/fs/btrfs/63152d54-3f28-408a-80a2-46e53b5c0bda/allocation/data/bytes_pinned/extra",
             "/tmp/63152d54-3f28-408a-80a2-46e53b5c0bda/allocation/data/bytes_pinned",
         ] {
