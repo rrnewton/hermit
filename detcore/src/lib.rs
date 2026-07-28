eb7e1f20c3da8ee598230dece827e4fb42bf55f1
Ryan Newton <rrnewton@users.noreply.github.com>
2026-07-28 15:44:43 -0700

    dbi: land shared coordinator and robust-list parity

--- detcore/src/lib.rs
+++ detcore/src/lib.rs
@@ -1224,7 +1224,9 @@ impl<T: RecordOrReplay> Tool for Detcore<T> {
         }
 
         // Except for the root task, let's block until it's our turn to go:
-        let th = tool_global::thread_start_request(&self.cfg, guest, self.detpid).await;
+        // TODO-HUMAN-REVIEW(PR-1141): Review child-state identity at DBI thread start.
+        let detpid = guest.thread_state().detpid.expect("detpid unset");
+        let th = tool_global::thread_start_request(&self.cfg, guest, detpid).await;
 
         // Finish the delayed initialization of the full threadstate:
         {
