d344a5ea5b400790ce66103eb954072a3eaf32cb
Ryan Newton <rrnewton@users.noreply.github.com>
2026-08-01 15:27:21 -0700

    Preserve LiteInst arch-prctl GS state

--- detcore/src/lib.rs
+++ detcore/src/lib.rs
@@ -1169,6 +1169,7 @@ impl<T: RecordOrReplay> Tool for Detcore<T> {
                 ThreadState {
                     dettid,
                     detpid: None, // Initialized later.
+                    arch_prctl_gs_shadow: pts.1.arch_prctl_gs_shadow,
                     open_file_creator: None,
                     mm_id: MmId::for_clone(
                         pts.1.mm_id,
